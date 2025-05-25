2e092a1b25d69309007aad43e90723e8b8a1443f
abbasn39 <abbasn39@gmail.com>
2025-05-26 00:45:26 +0500

    File added: 'Def' exercises

---
+++ 'Def' Exercises.py
@@ -0,0 +1,63 @@
+#1 Make a function that check is the number in the function is odd
+
+
+# def is_odd(x):
+#     return x % 2 == 1
+# s=int(input("Enter the number\n"))
+# is_odd(s)
+# print(is_odd(s))
+
+
+#2 Make a program that converts celsius into Fahrenheit, get a input from user
+
+# def C2F(x):
+#     return (x * (9/5))+32
+#
+# Celsius=int(input("Enter the Celsius Temperature please:"))
+# print(C2F(Celsius))
+#
+
+#3 Make a program that gets the First Character of a word, get a input from user
+
+# def first_char(first):
+#     return first[0]
+# while True:
+#     word=input("Please enter a word: \n")
+#     print(first_char(word))
+#     continue
+
+
+#4 Make a program that checks maximum of three numbers
+
+# def largest(x,y,z):
+#     if x>=y and x>=z:
+#         return x
+#     elif y>=z and y>=x:
+#         return y
+#     elif z>=x and z>=y:
+#         return z
+#     else:
+#         return None
+#
+# a=int(input("Enter the first number:\n"))
+# b=int(input("Enter the second number:\n"))
+# c=int(input("Enter the third number:\n"))
+#
+# L= largest(a,b,c)
+# print("The largest number is ",L)
+
+
+#5- Make a program that checks if a work is a palindrome, get input from user.
+#
+# def palindrome(check):
+#     if check == check[::-1]:
+#         print(f"The word '{check}' is a palindrome")
+#     else:
+#         print(f"The word '{check}' is not a palindrome")
+#
+# word = input("Please enter the word you want to check:\n")
+# palindrome(word)
+
+
+
+
